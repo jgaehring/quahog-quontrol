e2eb3ecbe99afa2657deee1e31ceb4418d00a879
Jamie Gaehring <gaehj457@gmail.com>
2019-07-31 19:23:42 -0400

    add basic index.html file and json response

--- src/quahog_quontrol/handler.clj
+++ src/quahog_quontrol/handler.clj
@@ -1,11 +1,28 @@
 (ns quahog-quontrol.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.util.response :refer [response content-type]]
+            [ring.middleware.json :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn wrap-dir-index [handler]
+  (fn [req]
+    (handler
+      (update
+        req
+        :uri
+        #(if (= "/" %) "/index.html" %)))))
+
+(def data [{:title "123"}
+           {:title "abc"}])
+
 (defroutes app-routes
-  (GET "/" [] "<h1>Welcome to Quahog Quontrol!</h1>")
+  (GET "/q" [] (content-type (response data) "application/json"))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults site-defaults)
+      (json/wrap-json-body)
+      (json/wrap-json-response)
+      (wrap-dir-index)))
